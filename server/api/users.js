d00b93e3343b2db7bf6aef19f46e5c80f48b53d6
Lani Tran <lanitran7@gmail.com>
2020-05-13 11:41:35 -0400

    created users route to get userId and user's parties

--- server/api/users.js
+++ server/api/users.js
@@ -1,16 +1,26 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Party} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+// GET /api/users/:userId
+router.get('/:userId', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
+    const user = await User.findByPk(req.params.userId)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET /api/users/:userId/parties
+router.get('/:userId/parties', async (req, res, next) => {
+  try {
+    const parties = await Party.findAll({
+      where: {
+        userId: req.params.userId
+      }
     })
-    res.json(users)
+    res.json(parties)
   } catch (err) {
     next(err)
   }
