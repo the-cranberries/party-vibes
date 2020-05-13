6bd37ee2e69bbf06d2090ca2aea42239a2e16ac7
Arielle Domantay <domantayarielle@gmail.com>
2020-05-13 17:03:44 -0400

    cleaning up

--- client/components/Chat.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const Chat = () => {
-  return <h1>WIP Chat Feature Page</h1>
-}
--- client/components/Guest-Join.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
-const Join = () => {
-  return <h1>WIP Guest Join Page</h1>
-}
