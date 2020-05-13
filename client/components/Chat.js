ef44d2af069a9d99c105d941867542b9d3112d43
Arielle Domantay <domantayarielle@gmail.com>
2020-05-13 14:26:42 -0400

    creating Chat and Join components

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,2 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const Chat = () => {
+  return <h1>WIP Chat Feature Page</h1>
+}
---
+++ client/components/Guest-Join.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Join = () => {
+  return <h1>WIP Guest Join Page</h1>
+}
