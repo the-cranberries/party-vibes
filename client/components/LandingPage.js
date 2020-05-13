28dde6c57f55c2163eca733d4d86ebe76d561c68
Lani Tran <lanitran7@gmail.com>
2020-05-13 11:57:32 -0400

    made new LandingPage component

---
+++ client/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <h1>Party Vibes</h1>
+        <img src="" />
+        <button type="button">Become a Host</button>
+        <button type="button">Join as a Host</button>
+        <button type="button">Join as a Guest</button>
+      </main>
+    </div>
+  )
+}
+
+export default Home
