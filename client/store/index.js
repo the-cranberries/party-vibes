2041a159519f63ac2ce58e8d4e380ae046b000e8
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-14 19:26:46 -0400

    added guests reducer to main reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,12 +3,15 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import guests from './guests'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, guests})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
+
+console.log(store.getState())
 
 export default store
 export * from './user'
