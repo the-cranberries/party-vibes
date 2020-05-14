426cc3db4e14a6c22c3b728b674ab4c114a9c499
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-14 19:23:55 -0400

    created addGuest action in guests reducer

---
+++ client/store/guests.js
@@ -0,0 +1,19 @@
+//action types
+const ADD_GUEST = 'ADD_GUEST'
+
+//action creators
+const addGuest = guest => ({
+  type: ADD_GUEST,
+  guest
+})
+
+//reducer
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case ADD_GUEST:
+      return [...state, action.guest]
+    default:
+      return state
+  }
+}
