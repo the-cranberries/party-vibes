bafd9deed9679d9bd7fbd741015cc17b5f53f1ba
Lani Tran <lanitran7@gmail.com>
2020-05-14 02:04:45 -0400

    added class attributes to the links

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import {Route, Switch, Link, Redirect} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
 import {Login, Signup} from './auth-form'
 
 const Home = () => {
   return (
     <div>
       <main>
-        {/* <button type="button">Become a Host</button>
-        <button type="button">Join as a Host</button>
-        <button type="button">Join as a Guest</button> */}
-
-        <Link to="/signup">Become a Host</Link>
-        <Link to="/parties/">Join as a Guest</Link>
-        <Link to="/login">Join as a Host</Link>
+        <Link className="linkBtn" to="/signup">
+          Become a Host
+        </Link>
+        <Link className="linkBtn" to="/">
+          Join as a Guest
+        </Link>
+        <Link className="linkBtn" to="/login">
+          Login as a Host
+        </Link>
 
         <Switch>
           <Route path="/signup" component={Signup} />
-          {/* <Route path='' component={}/> */}
+          {/* Add guest login component here */}
           <Route path="/login" component={Login} />
         </Switch>
       </main>
