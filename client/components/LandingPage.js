e9aab511e1e551f2902993b56c99b3917fedbdfd
Lani Tran <lanitran7@gmail.com>
2020-05-17 20:57:06 -0400

    changed colors and positioning of link buttons (WIP)

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -4,17 +4,25 @@ import {Login, Signup} from './auth-form'
 
 const Home = () => {
   return (
-    <div>
-      <main>
-        <Link className="linkBtn" to="/signup">
-          Become a Host
-        </Link>
-        <Link className="linkBtn" to="/login">
-          Login as a Host
-        </Link>
-        <Link className="linkBtn" to="/guestlogin">
-          Join as a Guest
-        </Link>
+    <div className="">
+      <main className="container text-center">
+        <div className="row">
+          <div className="col">
+            <Link className="linkBtn" to="/signup">
+              Become a Host
+            </Link>
+          </div>
+          <div className="col">
+            <Link className="linkBtn" to="/login">
+              Login as a Host
+            </Link>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <Link className="linkBtn" to="/guestlogin">
+            Join as a Guest
+          </Link>
+        </div>
       </main>
     </div>
   )
