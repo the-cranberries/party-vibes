207f60b0515c57562960db66f7b49d2a310f8e9f
Lani Tran <lanitran7@gmail.com>
2020-05-18 01:16:55 -0400

    created new Signup component to separate from Login component

---
+++ client/components/Signup.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {auth} from '../store'
+
+/**
+ * COMPONENT
+ */
+const SignupForm = props => {
+  const {name, displayName, handleSubmit, error} = props
+
+  return (
+    <div className="text-center">
+      <form onSubmit={handleSubmit} name={name}>
+        <div className="joinOuterContainer">
+          <h1 className="heading">Become a Host</h1>
+          <div className="row form-width mx-auto">
+            <label className="col-sm-3" htmlFor="hostName">
+              <medium>Name</medium>
+            </label>
+            <input
+              className="form-control col-sm-9"
+              name="hostName"
+              type="text"
+            />
+          </div>
+          <br />
+          <div className="row form-width mx-auto">
+            <label className="col-sm-3" htmlFor="profilePicture">
+              <medium>Choose a Profile Picture</medium>
+            </label>
+            <input
+              className="form-control col-sm-9"
+              name="profilePicture"
+              type="file"
+              accept="image/*"
+              multiple="false"
+            />
+          </div>
+          <br />
+          <div className="row form-width mx-auto">
+            <label className="col-sm-3" htmlFor="email">
+              <medium>Email</medium>
+            </label>
+            <input className="form-control col-sm-9" name="email" type="text" />
+          </div>
+          <br />
+          <div className="row form-width mx-auto">
+            <label className="col-sm-3" htmlFor="password">
+              <medium>Password</medium>
+            </label>
+            <input
+              className="form-control col-sm-9"
+              name="password"
+              type="password"
+            />
+          </div>
+          <br />
+          <div className="row">
+            <div className="col">
+              <button className="btn aqua-btn" type="submit">
+                {displayName}
+              </button>
+            </div>
+          </div>
+          {error &&
+            error.response && (
+              <div id="errorMessage">
+                <br /> {error.response.data}{' '}
+              </div>
+            )}
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const mapSignup = state => {
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.user.error
+  }
+}
+
+const mapDispatchSignup = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      const hostName = evt.target.hostName.value
+      const profilePicture = evt.target.profilePicture.value
+      dispatch(auth(email, password, formName, hostName, profilePicture))
+    }
+  }
+}
+
+export const Signup = connect(mapSignup, mapDispatchSignup)(SignupForm)
+
+/**
+ * PROP TYPES
+ */
+SignupForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object
+}
