6a7cacfcae24e4fb0dae745202c92c7635ef1ebe
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-18 16:53:51 -0400

    imported fetchUserParty thunk into HostDashboard component

--- client/components/HostDashboard.js
+++ client/components/HostDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchUserParty} from '../store/user'
 
 class HostDashboard extends React.Component {
   componentDidMount() {
