e530b15e0c21b37dcf040fc38aeed43810e7e1a4
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-20 15:26:23 -0400

    added logout button to HostDashboard after-party-create view

--- client/components/HostDashboard.js
+++ client/components/HostDashboard.js
@@ -138,6 +138,11 @@ class HostDashboard extends React.Component {
               End Party
             </button>
           </p>
+          <div>
+            <button type="button" onClick={handleClick}>
+              Log Out
+            </button>
+          </div>
         </div>
       )
     }
